07200571e70d456c4fae3f847c487358f7299de9
Abishek M S <108707094+AbishekMS@users.noreply.github.com>
2025-08-08 13:15:48 +0530

    Delete PracticeSessionOne directory

--- PracticeSessionOne/src/main/java/com/example/PracticeSessionOne/Contro.java
+++
@@ -1,9 +0,0 @@
-package com.example.PracticeSessionOne;
-
-public class Contro {
-	
-	public void gameDetails() {
-		System.out.println("Two soldiers shoot alien creatures. Remembers my childhood.");
-	}
-
-}
--- PracticeSessionOne/src/main/java/com/example/PracticeSessionOne/MarioGame.java
+++
@@ -1,8 +0,0 @@
-package com.example.PracticeSessionOne;
-
-public class MarioGame {
-  
-	public void gameDetails() {
-		System.out.println("MarioGame is otherwise known as super-mario");
-	}
-}
