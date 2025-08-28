911c88a6cce078c1d712c659660c57a522d6512a
Abishek M S <108707094+AbishekMS@users.noreply.github.com>
2025-08-28 20:17:08 +0530

    Update VersioningPersonController.java

--- URL Versioning- Rest API/src/main/java/com/example/PracticeTwo/VersioningPersonController.java
+++ URL Versioning- Rest API/src/main/java/com/example/PracticeTwo/VersioningPersonController.java
@@ -14,4 +14,22 @@ public class VersioningPersonController {
 	public PersonV2 getSecondVersionController() {
 		return new PersonV2(new Name("Rick","Grimes"));
 	}
+	
+	@GetMapping(path = "/person", params = "version=1")
+	public PersonV1 getRequestParam1Controller() {
+		return new PersonV1("Rick Grimes");
+	}
+	@GetMapping(path = "/person", params = "version=2")
+	public PersonV2 getRequestParam2Controller() {
+		return new PersonV2(new Name("Rick","Version2"));
+	}
+	
+	@GetMapping(path="/person", headers="X-API-VERSION=2")
+	public PersonV2 getPersonV2RequestHeader() {
+		return new PersonV2(new Name("Rick","header"));
+	}
+	@GetMapping(path="/person", headers="X-API-VERSION=1")
+	public PersonV1 getPersonV1RequestHeader() {
+		return new PersonV1("Rick Grimes");
+	}
 }
